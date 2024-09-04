2346cc57c58fb9e2900a535b0b016890defbef23
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2024-09-03 21:58:39 -0600

    add debug

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  //eslint-disable-next-line
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('debug') 
+  postDebugMsg(@Body() body: any) {
+    console.log('body:', body);
+    return body;
   }
 }
